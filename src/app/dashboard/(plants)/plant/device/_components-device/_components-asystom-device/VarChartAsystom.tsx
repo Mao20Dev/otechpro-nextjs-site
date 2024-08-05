147eee473b613c7abf2dd008a3aebcb4ebbcd5fe
Mao20Dev <uribe222@hotmail.com>
2024-08-04 22:51:15 -0500

    dynamic icon on asystom

--- src/app/dashboard/(plants)/plant/device/_components-device/_components-asystom-device/VarChartAsystom.tsx
+++ src/app/dashboard/(plants)/plant/device/_components-device/_components-asystom-device/VarChartAsystom.tsx
@@ -5,11 +5,28 @@ import React, { useState } from 'react'
 
 
 import {  useSearchParams } from 'next/navigation';
-import { AudioLines } from 'lucide-react';
+
+import { AudioLines, Gauge, Thermometer, Radio, Waves } from 'lucide-react'; // Asegúrate de importar todos los íconos necesarios
+
 
 import dynamic from 'next/dynamic';
 const Graph = dynamic(() => import('../Chart'), { ssr: false });
 
+
+const getIcon = (iconType: any) => {
+    switch (iconType) {
+        case 'vibration':
+            return <Radio className='text-zinc-200' size={30} />;
+        case 'speed':
+            return <Gauge className='text-zinc-200' size={30} />;
+        case 'temperature':
+            return <Thermometer className='text-zinc-200' size={30} />;
+        case 'frequency':
+            return <Waves className='text-zinc-200' size={30} />;
+        default:
+            return <AudioLines className='text-zinc-200' size={30} />; // Ícono por defecto
+    }
+};
 
 
 
@@ -35,7 +52,7 @@ function AsystomDevice({data, actualValue, unit, nameVariable, iconType}: any) {
             <div className='w-full h-full grid grid-cols-4 pb-0 xl:pb-12 xl:grid-cols-1 gap-4 rounded-2xl col-span-8 xl:col-span-1 justify-center items-center py-0 xl:py-2'>
                 <div className='w-full h-full flex items-center justify-start xl:justify-center xl:items-end col-span-1 xl:col-span-1'>
                     <div className='w-[60px] h-[60px] xl:w-[70px] xl:h-[70px] relative top-[-32px] ml-2  rounded-2xl bg-button-green-gradient flex flex-row justify-center items-center xl:static'> 
-                        <AudioLines className='text-zinc-200 ' size={30} /> 
+                        {getIcon(iconType)}  
                     </div>
                 </div>
                 <div className='col-span-3 h-full xl:col-span-1 flex flex-col justify-center items-end xl:justify-start xl:items-center pb-2'>
